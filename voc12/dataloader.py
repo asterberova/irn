c60b15b28ae4541179e4f64e72354cd37def9a70
Adéla Šterberová <asterberova@gmail.com>
2021-11-29 10:13:59 +0100

    reshape error

--- voc12/dataloader.py
+++ voc12/dataloader.py
@@ -207,8 +207,9 @@ class VOC12ClassificationDatasetMSF(VOC12ClassificationDataset):
             else:
                 s_img = imutils.pil_rescale(img, s, order=3)
             s_img = self.img_normal(s_img)
-            s_img = s_img.convert("RGB")
-            s_img = imutils.HWC_to_CHW(s_img)
+            # s_img = s_img.convert("RGB")
+            stacked_img = np.stack((s_img,) * 3, axis=-1)
+            s_img = imutils.HWC_to_CHW(stacked_img)
             ms_img_list.append(np.stack([s_img, np.flip(s_img, -1)], axis=0))
         if len(self.scales) == 1:
             ms_img_list = ms_img_list[0]
@@ -261,8 +262,9 @@ class VOC12SegmentationDataset(Dataset):
             img = imutils.top_left_crop(img, self.crop_size, 0)
             label = imutils.top_left_crop(label, self.crop_size, 255)
 
-        img = img.convert("RGB")
-        img = imutils.HWC_to_CHW(img)
+        # img = img.convert("RGB")
+        stacked_img = np.stack((img,) * 3, axis=-1)
+        img = imutils.HWC_to_CHW(stacked_img)
 
         return {'name': name, 'img': img, 'label': label}
 
